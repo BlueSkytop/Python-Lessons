6720885345c7cd13adf39c0416c1f22b071ec951
olgakutnja <olgakutnja@gmail.com>
2026-06-10 22:59:02 +0300

    homework done

---
+++ Lessons/Lesson1/homework.py
@@ -0,0 +1,30 @@
+# 1. Створіть 6 змінних з будь-якими значенням
+from operator import truediv
+
+str_books = 'Fairy tales', 'novel','poems'
+str_car = 'bmw'
+num1 = 528
+num2 = 569
+cake = True
+hot_dog = False
+print(str_books, str_car, num1, num2, cake, hot_dog, sep='\n')
+
+#2. Створіть дві змінні, a та b, та присвойте їм будь-які числові значення (цілі або дробові).
+# Обчисліть їх суму, різницю і тд.
+# Виведіть результати кожної операції на екран з відповідними поясненнями.
+blue = 10
+yellow = 20
+print(blue + yellow)
+print(yellow - blue)
+print(blue * yellow)
+print(yellow / blue)
+print(yellow % blue)
+
+# Створіть змінну first_name та присвойте їй ваше ім'я.
+# Створіть змінну last_name та присвойте їй ваше прізвище.
+# Створіть змінну full_name, об'єднавши (конкатенувавши) ім'я та прізвище через пробіл.
+# Виведіть full_name на екран.
+first_name = 'Liubov'
+last_name = 'Saliy'
+full_name = (first_name + " " + last_name)
+print(full_name)
